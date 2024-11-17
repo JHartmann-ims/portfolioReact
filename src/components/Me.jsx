2ec2a521214671fbc9b386790a86f9a697fee3fe
Julian Hartmann <julian.hartmann@students.ksba.ch>
2024-11-17 03:21:41 +0100

    adjusted content such as text and added showcase dummy

--- src/components/Me.jsx
+++ src/components/Me.jsx
@@ -3,51 +3,47 @@ import portrait from "../assets/portraitJHA.jpeg";
 import github from "../assets/github.png";
 import linkedin from "../assets/linkedin.png";
 import youtube from "../assets/youtube.png";
-
-
+import Showcase from "./Showcase";
 
 const Me = () => {
   return (
     <div className="flex flex-grow">
       <div className="flex w-full">
-        <div className="lg:w-[50%] w-[90%] min-h-screen flex  m-0 mx-auto place-content-center">
+        <div className="lg:w-[850px] w-[90%] min-h-screen flex  m-0 mx-auto place-content-center">
           <div className="w-[90%] h-full">
             <img
               src={portrait}
               alt="portrait of Julian"
               className="rounded-full w-48 h-48 sm:w-64 sm:h-64 block ml-auto mr-auto border-solid border-4 border-neutral drop-shadow-md"
             />
-<div className="w-full sm:w-[90%] md:w-[80%] h-auto rounded-lg bg-neutral mt-5 mx-auto text-center p-4">
-  <h3 className="text-base sm:text-lg md:text-xl font-semibold">
-    Hello, I'm a guy who does application development!
-  </h3>
-</div>
+            <div className="w-full h-auto rounded-lg bg-neutral mt-5 mx-auto text-center p-4 shadow-md">
+              <h3 className="text-base sm:text-lg md:text-xl font-semibold">
+                Hello, I'm a guy who does software development!
+              </h3>
+            </div>
+            
+            <Showcase />
 
             <h1 className="pt-5 text-4xl font-bold">Julian Hartmann</h1>
-            <p>Student of computer science, based in Switzerland</p>
+            <p>Application developer, based in Switzerland</p>
             <div className="w-[100%] m-0 mx-auto pt-10">
-              <h2 className="text-3xl font-bold">What do I do ?</h2>
-              <p className="">
-                I am currently attending a school that is mostly involved in
-                application development. As a student there, I have gained
-                experience in various areas of computer science in the last two
-                years, which I look forward to expanding in the years to come.
-                When I'm not at work, I usually like to hang out with my friends, especially with my girlfriend
-                online and offline. Though it often happens that my friends and
-                I are suddenly discussing about our projects we are working on,
-                when we're online. Every project I did in school has been
-                documented by me and you can see some of my finished projects by
-                clicking the button below.
+              <h2 className="text-3xl font-bold">Who am I ?</h2>
+              <p>
+               I am a 20 year old developer with a strong passion for technology and software development, which began during my early years. Recently i graduated from the Informatikmittelschule in Baden, earning a Berufsmatura and an EFZ.
+               I have gained solid experience in both frontend and backend development, equipping me with the skills to tackle a wide range of challenges.
               </p>
-              <div className="text-center pt-3">
-                <a role="button" className="btn btn-wide" href="https://portfolio.bbbaden.ch/view/view.php?t=hJRKLrHia4WAlUx1MCPs" target="_blank" rel="noreferrer">My Projects</a>
-              </div>
+              <br/>
+              <p>
+              My journey into development started with a keen interest in understanding how things work behind the scenes-wether it was the functionality of websites, applications, or video games.
+               This curiosity led me to create my first video game using Unity and C#. This experience was needed to get me to realize that this is a career
+               I wanted to pursue. 
+              </p>
             </div>
             <div className="w-[100%] m-0 mx-auto pt-10">
               <h2 className="text-3xl font-bold">Bio</h2>
-              <div class="overflow-x-auto pt-2">
-                <table class="table w-full">
-                  <thead>
+              <div className="overflow-x-auto pt-2">
+                <table className="table w-full">
+                  <thead className="bg-base-300">
                     <tr>
                       <th>Year</th>
                       <th>Education</th>
@@ -55,7 +51,7 @@ const Me = () => {
                       <th>City</th>
                     </tr>
                   </thead>
-                  <tbody>
+                  <tbody className="bg-base-200">
                     <tr>
                       <th>2011-2017</th>
                       <td>Primary School</td>
@@ -71,10 +67,17 @@ const Me = () => {
                     </tr>
 
                     <tr>
-                      <th>2020-today</th>
+                      <th>2020-2024</th>
                       <td>Informatikmittelschule</td>
                       <td>Switzerland</td>
                       <td>Baden</td>
+                    </tr>
+
+                    <tr>
+                      <th>2023-today</th>
+                      <td>RTP GmbH</td>
+                      <td>Switzerland</td>
+                      <td>Zurich</td>
                     </tr>
                   </tbody>
                 </table>
@@ -82,103 +85,49 @@ const Me = () => {
             </div>
             <div className="w-[100%] m-0 mx-auto pt-10">
               <h2 className="text-3xl font-bold">
-                Knowledge<span className="text-base"> in computer science</span>
-              </h2>
-              <div className="grid grid-rows-4 grid-flow-col gap-4 text-white text-sm text-center font-bold leading-6 bg-stripes-pink rounded-lg pt-2">
-                <div className="p-4 rounded-lg shadow-ll bg-base-300">
-                  <p>Java</p>
-                  <progress
-                    class="progress progress-primary sm:w-56 w-32"
-                    value="55"
-                    max="100"
-                  ></progress>
-                </div>
-                <div className="p-4 rounded-lg shadow-ll bg-base-300">
-                  <p>.Net</p>
-                  <progress
-                    class="progress progress-primary sm:w-56 w-32"
-                    value="80"
-                    max="100"
-                  ></progress>
-                </div>
-                <div className="p-4 rounded-lg shadow-ll bg-base-300">
-                  <p>CSS</p>
-                  <progress
-                    class="progress progress-primary sm:w-56 w-32"
-                    value="75"
-                    max="100"
-                  ></progress>
-                </div>
-                <div className="p-4 rounded-lg shadow-ll bg-base-300">
-                  <p>SQL</p>
-                  <progress
-                    class="progress progress-primary sm:w-56 w-32"
-                    value="60"
-                    max="100"
-                  ></progress>
-                </div>
-                <div className="p-4 rounded-lg shadow-ll bg-base-300">
-                  <p>C#</p>
-                  <progress
-                    class="progress progress-primary sm:w-56 w-32"
-                    value="75"
-                    max="100"
-                  ></progress>
-                </div>
-                <div className="p-4 rounded-lg shadow-ll bg-base-300">
-                  <p>React</p>
-                  <progress
-                    class="progress progress-primary sm:w-56 w-32"
-                    value="45"
-                    max="100"
-                  ></progress>
-                </div>
-                <div className="p-4 rounded-lg shadow-ll bg-base-300">
-                  <p>HTML</p>
-                  <progress
-                    class="progress progress-primary sm:w-56 w-32"
-                    value="80"
-                    max="100"
-                  ></progress>
-                </div>
-                <div className="p-4 rounded-lg shadow-ll bg-base-300">
-                  <p>Javascript</p>
-                  <progress
-                    class="progress progress-primary sm:w-56 w-32"
-                    value="75"
-                    max="100"
-                  ></progress>
-                </div>        
-              </div>
-            </div>
-            <div className="w-[100%] m-0 mx-auto pt-10">
-              <h2 className="text-3xl font-bold">
                 I <span className="text-3xl">&#x2661;</span>
               </h2>
               <p className="">
-                basketball, music, playing my ukulele, web development
+                basketball, music, playing my ukulele, coding
               </p>
             </div>
             <div className="w-[100%] m-0 mx-auto pt-10">
               <h2 className="text-3xl font-bold pb-2">On the web</h2>
               <div class="mockup-window border bg-base-300">
                 <div class="grid justify-center px-4 py-16 bg-base-200 gap-2">
-                  <a href="https://github.com/JHartmann-ims" role="button" className="btn btn-outline btn-wide gap-2" target="_blank" rel="noreferrer">
-                    <img src={github} alt="" className="w-7 h-7"/>
+                  <a
+                    href="https://github.com/JHartmann-ims"
+                    role="button"
+                    className="btn btn-outline btn-wide gap-2"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img src={github} alt="" className="w-7 h-7" />
                     JHartmann-ims
                   </a>
-                  <a href="https://www.linkedin.com/in/julian-hartmann-a17999239/" role="button" className="btn btn-outline btn-wide gap-2" target="_blank" rel="noreferrer">
-                    <img src={linkedin} alt="" className="w-6 h-6"/>
+                  <a
+                    href="https://www.linkedin.com/in/julian-hartmann-a17999239/"
+                    role="button"
+                    className="btn btn-outline btn-wide gap-2"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img src={linkedin} alt="" className="w-6 h-6" />
                     Julian Hartmann
                   </a>
-                  <a href="https://www.youtube.com/channel/UCCQpiU3-tQ1gbrj7JdVyqKQ/videos" role="button" className="btn btn-outline btn-wide gap-2" target="_blank" rel="noreferrer">
-                    <img src={youtube} alt="" className="w-7 h-7"/>
+                  <a
+                    href="https://www.youtube.com/channel/UCCQpiU3-tQ1gbrj7JdVyqKQ/videos"
+                    role="button"
+                    className="btn btn-outline btn-wide gap-2"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <img src={youtube} alt="" className="w-7 h-7" />
                     quacksterr
                   </a>
                 </div>
               </div>
             </div>
-            
           </div>
         </div>
       </div>
