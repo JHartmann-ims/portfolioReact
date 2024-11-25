0d5351756bc548f044266b0111f801973bfe5249
Julian hartmann <jha@rtp.ch>
2024-11-25 19:18:30 +0100

    removed old email and added new one

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
                 <h1 className='text-3xl sm:text-4xl sm:pl-5 font-bold pl-10'>&lt;JHartmann&gt;</h1>
             </div>
             <div className='sm:pr-6 pr-9'>
-                <a href="mailto:julian.hartmann@students.ksba.ch"><button className='btn flex'><MailIcon className='w-5 h-5' /></button></a>
+                <a href="mailto:jhartmann.dev04@gmail.com"><button className='btn flex'><MailIcon className='w-5 h-5' /></button></a>
             </div>
         </div>
     </div>
